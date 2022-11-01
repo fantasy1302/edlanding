7fcd187f7ff3684e05aa6bfa26ee3bd9c9a8fd10
fantasy1302 <84225560+fantasy1302@users.noreply.github.com>
2022-11-01 14:18:49 +0400

    Delete hidegirl.js

--- hidegirl.js
+++
@@ -1,7 +0,0 @@
-const buttn =  document.getElementsById("submit");
-         const girl = document.getElementsByClassName("girlimage");
-         
-         buttn.addEventListener('click', function hd() {
-            document.getElementById("submit").style.visibility = "hide"; // hide
-    
-            });
